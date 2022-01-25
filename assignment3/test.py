95b7c4a79da16bda8d4a7ea4278290d6d550b743
marijn111 <39378193+marijn111@users.noreply.github.com>
2022-01-25 11:39:28 +0100

    Update test.py

--- assignment3/test.py
+++ assignment3/test.py
@@ -1,9 +1,7 @@
 import spacy
-import textacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp("I have not been able to do this in years.")
 
-ngrams = list(textacy.extract.basics.ngrams(doc, 2, min_freq=2))
-
-print(ngrams)
+for token in doc:
+    print(token.dep_)
